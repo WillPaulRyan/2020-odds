b1411bb4cd844d95fcc099055be472d6f3a040de
Will Ryan <willpaulryan@gmail.com>
2020-02-06 14:47:09 +0100

    Flask Environment Variables testing

--- helpers.py
+++ helpers.py
@@ -4,7 +4,7 @@ from operator import itemgetter
 def candidateSort(A):
     """Sort the candidates by probability and add information"""
 
-    B = [['./static/candidates/trump.png', 'Donald Trump', A[1]], ['./static/candidates/warren.png', 'Elizabeth Warren', A[2]], ['./static/candidates/booker.png', 'Cory Booker', A[3]], ['./static/candidates/biden.png', 'Joe Biden', A[4]], ['./static/candidates/sanders.png', 'Bernie Sanders', A[5]], ['./static/candidates/klobuchar.png', 'Amy Klobuchar', A[6]], ['./static/candidates/harris.png', 'Kamala Harris', A[7]], ['./static/candidates/gillibrand.png', 'Kirsten Gillibrand', A[8]], ['./static/candidates/gabbard.png', 'Tulsi Gabbard', A[9]], ['./static/candidates/orourke.png', "Beto O'Rourke", A[10]], ['./static/candidates/yang.png', 'Andrew Yang', A[11]], ['./static/candidates/buttigieg.png', 'Pete Buttigieg', A[12]], ['./static/candidates/castro.png', 'Julián Castro', A[13]]]
+    B = [['./static/candidates/trump.png', 'Donald Trump', A[1]], ['./static/candidates/warren.png', 'Elizabeth Warren', A[2]], ['./static/candidates/booker.png', 'Cory Booker', A[3]], ['./static/candidates/biden.png', 'Joe Biden', A[4]], ['./static/candidates/sanders.png', 'Bernie Sanders', A[5]], ['./static/candidates/klobuchar.png', 'Amy Klobuchar', A[6]], ['./static/candidates/harris.png', 'Kamala Harris', A[7]], ['./static/candidates/gillibrand.png', 'Kirsten Gillibrand', A[8]], ['./static/candidates/gabbard.png', 'Tulsi Gabbard', A[9]], ['./static/candidates/orourke.png', "Beto O'Rourke", A[10]], ['./static/candidates/yang.png', 'Andrew Yang', A[11]], ['./static/candidates/buttigieg.png', 'Pete Buttigieg', A[12]], ['./static/candidates/castro.png', 'Julian Castro', A[13]]]
 
     return A[0], sorted(B, key=itemgetter(2))
 
